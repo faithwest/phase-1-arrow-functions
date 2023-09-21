32249756d2b033e7df0b9ea0e59b511cb3c12858
faithwest <faithchepkoech425@gmail.com>
2023-09-21 18:48:34 +0300

    complete

---
+++ index.js
@@ -0,0 +1,10 @@
+const divide = (a, b) => a / b;
+divide(2000,100);
+
+const nums = [1,2,3];
+const square = nums.map(x => x ** 2); 
+square; //=> [1,4,9]
+nums;
+
+const add = (parameter1, parameter2) => parameter1 + parameter2;
+add(3,4);
